d67fee67a960207fc6852543bde58b48f03a60e0
Dev-Shakil <shakilhossain1007@gmail.com>
2024-05-23 19:27:53 +0600

    image upload

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -21,7 +21,8 @@
 
     <div class="flex items-center flex-wrap  justify-between w-full px-3 mt-3" href="#">
         <div class="flex items-center flex-col justify-center w-full">
-            <img src="{{ asset('public/' . Auth::user()->company_logo) }}" alt="logo" width="100px" height="69px"/>
+            {{-- <img src="{{ 'public/' . Auth::user()->company_logo }}" alt="logo" width="100px" height="69px"/> --}}
+            <img src="{{ url(Auth::user()->company_logo)}}" alt="logo" width="100px" height="69px"/>
             <span class="ml-2 text-sm font-bold">{{ Auth::user()->name }}</span>
             {{-- <p class="ml-2 text-sm font-bold w-full">{{ Auth::user()->company_logo }}</p> --}}
         </div>
